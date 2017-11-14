8123ae0f5490100f8367a9e667e9b94063086037
minekan <minekan.yama04@gmail.com>
2017-11-14 23:37:16 +0900

    add ajax

--- SAStrutsTest/src/main/java/sample/sastruts/test/action/DbTestAction.java
+++ SAStrutsTest/src/main/java/sample/sastruts/test/action/DbTestAction.java
@@ -1,12 +1,18 @@
 package sample.sastruts.test.action;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.seasar.struts.annotation.ActionForm;
 import org.seasar.struts.annotation.Execute;
+import org.seasar.struts.util.ResponseUtil;
 
+import net.arnx.jsonic.JSON;
 import sample.sastruts.test.entity.IdTest;
 import sample.sastruts.test.form.DbTestForm;
 import sample.sastruts.test.service.IdTestService;
@@ -16,6 +22,9 @@ public class DbTestAction
 	@ActionForm
 	@Resource
 	public DbTestForm dbTestForm;
+
+	@Resource
+	HttpServletRequest req;
 
 	@Resource
 	public IdTestService idTestService;
@@ -28,7 +37,22 @@ public class DbTestAction
     	resultList = idTestService.findLikeId(dbTestForm.getId());
         return "index.jsp";
     }
+    @Execute(validator = false,urlPattern="load")
+    public String load_json() throws IOException {
+    	String id = req.getParameter("id");
+        //データベースからデータを全件取得します。
+    	List<IdTest> result = idTestService.findLikeId(id);
 
+        HttpServletResponse httpServletResponse = ResponseUtil.getResponse();
+        httpServletResponse.setContentType("application/json");
+        PrintWriter sendPoint = new PrintWriter(httpServletResponse.getOutputStream());
+
+        //Entity→JSON形式に変換して出力します。
+        sendPoint.println(JSON.encode(result));
+        sendPoint.flush();
+        sendPoint.close();
+        return null;
+    }
     @Execute(validator = false,urlPattern="newData")
     public String newData()
     {
--- SAStrutsTest/src/main/webapp/WEB-INF/view/dbTest/index.jsp
+++ SAStrutsTest/src/main/webapp/WEB-INF/view/dbTest/index.jsp
@@ -18,17 +18,40 @@ $(function(){
 		  $(this).parents('form').attr('action', '/SAStrutsTest/dbTest/newData');
 		  $(this).parents('form').submit();
 	  });
+	  $('[name="load"]').click(function() {
+		  $.ajax({
+			    url: '${f:url("/dbTest/load")}',
+			    type: 'GET',
+			    data :{'id': $('[name="id"]').val()},
+			    dataType: 'json',
+			    cache: false, //ブラウザにキャッシュさせません。
+			    error: function(){
+			        alert('DBの読み込みに失敗しました');
+			    },
+			    success: function(json){
+			        $('#resultList').html('');
+			        for( var i=0; i<json.length; i++ ) {
+			            $('#resultList').append('<tr>');
+			            $('#resultList').append('<td><input type="button" name="detail" value="更新" /></td>');
+			            $('#resultList').append('<td name="id">' + json[i].id + '</td>');
+			            $('#resultList').append('<td name="val">' + json[i].val + '</td>');
+			            $('#resultList').append('</tr>');
+			        }
+			    }
+			});
+		});
 	});
 
 </script>
 <body>
 <s:form>
     key:<html:text property="id"/><br/>
+    <input type="button" name="load" value="一覧更新" />
     <input type="button" name="newData" value="新規登録" />
     <input type="submit" name="index" value="検索" />
     <br/>
     <br/>
-	<table border="1">
+	<table border="1" id="resultList">
 	<c:forEach var="item" items="${ resultList }">
 		<tr>
 		<td><input type="button" name="detail" value="更新" /></td>
